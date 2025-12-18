aba9836d7671abf36dceeb6e4e1e63885a9634b5
huyht101 <85950651+huyht101@users.noreply.github.com>
2025-12-18 22:16:15 +0700

    Init 3

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "2004",
+  password: "363n*j0I0F5q0&N^&27P3#x65",
   database: "applestore",
 });
 
